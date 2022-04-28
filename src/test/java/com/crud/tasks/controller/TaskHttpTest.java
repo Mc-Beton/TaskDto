f72f971b05db3fe7abcc971cd6528cc963d47511
Filip Gabrys <el.filberto@gmail.com>
2022-04-28 13:46:33 +0200

    fix tests

--- src/test/java/com/crud/tasks/controller/TaskHttpTest.java
+++ src/test/java/com/crud/tasks/controller/TaskHttpTest.java
@@ -4,6 +4,7 @@ import com.crud.tasks.config.CoreConfiguration;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
 import com.google.gson.Gson;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -31,13 +32,19 @@ class TaskHttpTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private TaskController taskController;
+    private DbService service;
+
+    @MockBean
+    private TaskMapper mapper;
 
     @Test
     void getAllTasksTest() throws Exception {
         //Given
         List<TaskDto> tasksDto = List.of(new TaskDto(1L, "Title", "Content"));
-        when(taskController.getTasks()).thenReturn(ResponseEntity.ok(tasksDto));
+        List<Task> tasks = List.of(new Task(1L, "Title", "Content"));
+
+        when(service.getAllTasks()).thenReturn(tasks);
+        when(mapper.mapToTaskDtoList(tasks)).thenReturn(tasksDto);
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/v1/tasks")
@@ -51,9 +58,12 @@ class TaskHttpTest {
     @Test
     void getTaskWithSpecifiedIdTest() throws Exception {
         //Given
-        TaskDto taskDto = new TaskDto(1L, "Title", "Content");
+        TaskDto tasksDto = new TaskDto(1L, "Title", "Content");
+        Task tasks = new Task(1L, "Title", "Content");
 
-        when(taskController.getTask(1L)).thenReturn(ResponseEntity.ok(taskDto));
+        when(service.getTask(1L)).thenReturn(tasks);
+        when(mapper.mapToTaskDto(tasks)).thenReturn(tasksDto);
+
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/v1/tasks/1")
@@ -67,9 +77,12 @@ class TaskHttpTest {
     void createTaskTest() throws Exception {
         //Given
         TaskDto taskDto = new TaskDto(1L, "Title", "Content");
-
+        Task tasks = new Task(1L, "Title", "Content");
         Gson gson = new Gson();
         String jsonContent = gson.toJson(taskDto);
+
+        when(mapper.mapToTask(taskDto)).thenReturn(tasks);
+        when(service.saveTask(tasks)).thenReturn(tasks);
 
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
@@ -84,9 +97,13 @@ class TaskHttpTest {
     void updateTaskTest() throws Exception {
         //Given
         TaskDto taskDto = new TaskDto(1L, "Title", "Content");
-        when(taskController.updateTask(taskDto)).thenReturn(ResponseEntity.ok(taskDto));
+        Task tasks = new Task(1L, "Title", "Content");
         Gson gson = new Gson();
         String jsonContent = gson.toJson(taskDto);
+
+        when(mapper.mapToTask(taskDto)).thenReturn(tasks);
+        when(service.saveTask(tasks)).thenReturn(tasks);
+        when(mapper.mapToTaskDto(tasks)).thenReturn(taskDto);
 
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
@@ -101,7 +118,6 @@ class TaskHttpTest {
     @Test
     void deleteTaskTest() throws Exception {
         //Given
-        when(taskController.deleteTask(1L)).thenReturn(ResponseEntity.ok().build());
         //When & Then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/v1/tasks/1")
